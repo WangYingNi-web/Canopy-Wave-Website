fe6106ea1283c0a63e19e9f025f8cc171f2e03bc
WangYingNi-web <wangyingni@canopywave.com>
2025-04-23 14:39:23 +0800

    修改

--- public/page.tsx
+++ public/page.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from "@/components/ui/button"
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import {
   Carousel,
   CarouselContent,
