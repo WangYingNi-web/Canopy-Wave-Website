e901a725bd62df1cdfe999e383d6d4cd1e519868
WangYingNi-web <wangyingni@canopywave.com>
2025-04-23 20:01:33 +0800

    修改报错文件

--- src/hooks/useScrollToHash.ts
+++ src/hooks/useScrollToHash.ts
@@ -32,5 +32,5 @@ export const useScrollToHash = () => {
       window.removeEventListener('load', handleScroll);
       router.events.off('hashChangeComplete', handleScroll);
     };
-  }, [router.asPath]);
+  }, [router.events]);
 };
