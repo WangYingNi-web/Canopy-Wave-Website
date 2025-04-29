b7eca4d35ecd2717500e0580840cfe79cca8b07b
WangYingNi-web <wangyingni@canopywave.com>
2025-04-29 16:26:36 +0800

    添加表单接口

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div id="root">
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Component {...pageProps} />
+        <Toaster />
       </ThemeProvider>
     </div>
   )
