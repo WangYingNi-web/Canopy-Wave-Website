f685e53d4db7a5924516c2135998ba6ecaa85041
WangYingNi-web <wangyingni@canopywave.com>
2025-07-31 16:41:38 +0800

    修改行间距

--- src/pages/resources/tutotrials/index.tsx
+++ src/pages/resources/tutotrials/index.tsx
@@ -27,8 +27,8 @@ const Tutotrials: React.FC = () => {
           <div className="absolute inset-0 z-10">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-[160px]">
               <SlideUp>
-                <h1 className="text-4xl sm:text-5xl font-black text-[#80b224] mb-6 leading-tight">
-                How to Run DeepSeek-R1 <br /> Locally in a Canopy Wave VM?
+                <h1 className="text-4xl sm:text-5xl sm:leading-[1.2] font-black text-[#80b224] leading-tight">
+                  How to Run DeepSeek-R1 <br /> Locally in a Canopy Wave VM?
                 </h1>
               </SlideUp>
             </div>
