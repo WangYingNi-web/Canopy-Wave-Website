ba219f2b5bb8b1df4f832b464fb1d82489063128
WangYingNi-web <wangyingni@canopywave.com>
2025-08-27 18:27:39 +0800

    修改3

--- src/components/tutorials/TutorialLayout5.tsx
+++ src/components/tutorials/TutorialLayout5.tsx
@@ -640,7 +640,7 @@ const TutorialLayout5: React.FC = () => {
             {/* Banner Section */}
             <div className="w-full h-[520px] relative mt-[84px]">
                 <Image
-                    src="/tutorials/workload/banner.png"
+                    src="/blog/workflows.png"
                     alt="AI Storage Tutorial Banner"
                     fill
                     className="object-cover"
