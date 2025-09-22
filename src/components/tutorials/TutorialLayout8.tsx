c3a1d989b4b3ef469b2e70d38c81178308cdd0af
WangYingNi-web <wangyingni@canopywave.com>
2025-09-22 15:25:31 +0800

    修改vs

--- src/components/tutorials/TutorialLayout8.tsx
+++ src/components/tutorials/TutorialLayout8.tsx
@@ -197,7 +197,7 @@ const TutorialLayout8: React.FC = () => {
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-44">
                         <SlideUp>
                             <h1 className="text-4xl sm:text-5xl sm:leading-[1.2] font-black text-[#80b224]">
-                                Comparing Al Agent Frameworks: <br /> LangGraphvs AutoGPTvs AgentGPT
+                                Comparing Al Agent Frameworks: <br /> LangGraph vs AutoGPT vs AgentGPT
                             </h1>
                         </SlideUp>
                     </div>
